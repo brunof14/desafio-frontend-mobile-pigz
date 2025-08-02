3355c7a91c4a319b9f7dc3d6a6e87858d42468ec
Bruno Felipe <oliveiraf.120@gmail.com>
2025-08-02 16:17:19 -0400

    feat(components): update svgs components for path recive fill color via props

--- components/assets/TicketSvg.tsx
+++ components/assets/TicketSvg.tsx
@@ -4,7 +4,7 @@ const TicketSvg = (props: SvgProps) => (
   <Svg width={28} height={28} {...props}>
     <G data-name="Grupo 3684">
       <Path
-        fill="#9f9f9f"
+        fill={props.color ?? "#9f9f9f"}
         d="M15 17.997H9a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2Zm-4-6h2a1 1 0 0 0 0-2h-2a1 1 0 0 0 0 2Zm12 2h-3v-9a1 1 0 0 0-1.5-.87l-3 1.72-3-1.72a1 1 0 0 0-1 0l-3 1.72-3-1.72a1 1 0 0 0-1.5.87v16a3 3 0 0 0 3 3h14a3 3 0 0 0 3-3v-6a1 1 0 0 0-1-1Zm-16 8a1 1 0 0 1-1-1V6.727l2 1.14a1.08 1.08 0 0 0 1 0l3-1.72 3 1.72a1.08 1.08 0 0 0 1 0l2-1.14v14.27a3 3 0 0 0 .18 1Zm15-1a1 1 0 0 1-2 0v-5h2Zm-7-7H9a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2Z"
       />
       <Path fill="none" d="M0 0h28v28H0z" data-name="Ret\xE2ngulo 5667" />
--- components/assets/UserSvg.tsx
+++ components/assets/UserSvg.tsx
@@ -4,7 +4,7 @@ const UserSvg = (props: SvgProps) => (
   <Svg width={28} height={28} {...props}>
     <G data-name="Grupo 3686">
       <Path
-        fill="#9f9f9f"
+        fill={props.color ?? "#9f9f9f"}
         d="M17.695 14.716a6 6 0 1 0-7.42 0 10 10 0 0 0-6.22 8.18 1.006 1.006 0 1 0 2 .22 8 8 0 0 1 15.9 0 1 1 0 0 0 1 .89h.11a1 1 0 0 0 .88-1.1 10 10 0 0 0-6.25-8.19Zm-3.71-.71a4 4 0 1 1 4-4 4 4 0 0 1-4 4Z"
       />
       <Path fill="none" d="M0 0h28v28H0z" data-name="Ret\xE2ngulo 5668" />

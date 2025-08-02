f3c633e96e832ce71847d4bbf10ee334e186e607
Bruno Felipe <oliveiraf.120@gmail.com>
2025-08-02 17:58:19 -0400

    fix: missing color

--- components/DeliverySummaryCard.tsx
+++ components/DeliverySummaryCard.tsx
@@ -51,6 +51,7 @@ const styles = StyleSheet.create({
     fontFamily: FontFamily.Poppins.bold,
     fontSize: 16,
     marginBottom: 16,
+    color: Colors.gray800,
   },
   cardsContainer: {
     flexDirection: "row",
